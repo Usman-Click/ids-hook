c0c7225c983517b077700fe0172cbd38b2efa202
Usman-Click <47116342+Usman-Click@users.noreply.github.com>
2025-08-10 20:55:12 +0100

    Add Resend integration for email verification and update dependencies

--- pages/api/send-code.js
+++ pages/api/send-code.js
@@ -1,5 +1,6 @@
 // 1. Import Firebase Admin
 import admin from "firebase-admin";
+import { Resend } from "resend";
 
 // 2. Initialize Firebase Admin SDK only once
 if (!admin.apps.length) {
@@ -16,23 +17,32 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
     if (data != null) {
-      const uid =data.user.uid;
+      const uid = data.user.uid;
       const snapshot = await admin
         .firestore()
         .collection("users")
         .doc(uid)
         .get();
 
-        const userEmail = snapshot.data.email;
+      // Generate 6-digit code
+      const code = Math.floor(100000 + Math.random() * 900000).toString();
+      const resend = new Resend("re_Roe1xZd8_6DVZMewNGYmSyZzF5Czt5Sb9");
+      const usermail = snapshot.email;
 
-        // Generate 6-digit code
-        const code = Math.floor(100000 + Math.random() * 900000).toString();
+      resend.emails.send({
+        from: "Intrusion Detection System",
+        to: "uadamuharuna@gmail.com",
+        subject: "Verification Code",
+        html:
+          "<p>Hi, Your Verification code is <strong>" + code + "</strong>!</p>",
+      });
 
-        console.log("Webhook received, Data:", userEmail + code);
-      }
-    
+      console.log("Webhook received, Data:", usermail + code);
+    }
 
-    return res.status(100).json({ message: "Event ignored" });
+    return res.status(200).json({ message: "Code Sent Succesfully" });
+
+    // return res.status(100).json({ message: "Event ignored" });
   } else {
     res.setHeader("Allow", ["POST"]);
     res.status(405).json({ message: `Method ${req.method} not allowed` });
