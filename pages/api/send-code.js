523f93028f2db8fe700442b453e56e66c63f6e71
Usman-Click <47116342+Usman-Click@users.noreply.github.com>
2025-08-11 09:57:04 +0100

    Add CORS headers for allowed methods and headers in send-code.js

--- pages/api/send-code.js
+++ pages/api/send-code.js
@@ -15,6 +15,8 @@ if (!admin.apps.length) {
 export default async function handler(req, res) {
   // Add CORS headers
   res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   //  Handle the preflight OPTIONS request
   if (req.method === "OPTIONS") {
